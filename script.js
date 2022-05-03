75bd7251451177da51c20a471b197a8a81d2e3fb
Ivan Chang <80505615+JustChangHTML5@users.noreply.github.com>
2022-05-03 14:52:35 +0000

    fffff

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ ctx.imageSmoothingEnabled = false;
 var loaded = false;
 var curMousePos = [0, 0];
 
+Math.dist=function(x1,y1,x2,y2){ 
+  if(!x2) x2=0; 
+  if(!y2) y2=0;
+  return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)); 
+}
+
+function rotate(cx, cy, x, y, angle) {
+    var radians = (Math.PI / 180) * angle,
+        cos = Math.cos(radians),
+        sin = Math.sin(radians),
+        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
+        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+    return [nx, ny];
+}
+
+function angle(cx, cy, ex, ey) {
+  var dy = ey - cy;
+  var dx = ex - cx;
+  var theta = Math.atan2(dy, dx); // range (-PI, PI]
+  theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+  //if (theta < 0) theta = 360 + theta; // range [0, 360)
+  return theta;
+}
+
+var bullets = [];
+
 document.onmousemove = handleMouseMove;
 function handleMouseMove(event) {
     var eventDoc, doc, body;
@@ -66,11 +92,48 @@ class Bullet {
         this.frameRate = skinSpeed;
         this.maxFrames = skinSpeed;
         this.loaded = false;
+        this.destroyed = false;
+    }
+
+    loadImage(url) {
+        let l = new Image();
+        l.src = url
+
+        return l;
+    }
+
+    loadImages(paths) {
+        for (var i = 0; i < paths.length; i++) {
+            var curPath = paths[i];
+            var image = this.loadImage(curPath);
+            this.sprites.push(image);
+        }
+    }
+
+    draw(angle) {
+        ctx.save();
+        ctx.translate(this.pos[0], this.pos[1]);
+        ctx.rotate(angle * Math.PI / 180);
+        ctx.drawImage(this.curImage, -this.skinData[0] / 2, - this.skinData[1] / 2, this.skinData[0], this.skinData[1]);
+        ctx.restore();
+        this.loaded = true;
+    }
+
+    update() {
+        if (this.destroyed) {
+            bullets.splice(bullets.find(this), 1);
+        }
+        
+        if (this.pos[0] > canvas.width || this.pos[0] < 0) {
+            this.destroyed = true;
+        } else if (this.pos[1] > canvas.height || this.pos[1] < 0) {
+            this.destroyed = true;
+        }
     }
 }
 
 class Player {
-    constructor(skin, skinData, stats, skinSpeed) {
+    constructor(skin, skinData, stats, skinSpeed, bulletSkins) {
         this.sprites = [];
         this.skin = skin;
         this.loadImages(skin);
@@ -79,10 +142,11 @@ class Player {
         this.skinData = skinData
         this.stats = stats;
         this.pos = [0, 0];
-        this.vel = [];
+        this.direction = 0;
         this.frameRate = skinSpeed;
         this.maxFrames = skinSpeed;
         this.loaded = false;
+        this.bulletSkins = bulletSkins;
     }
     
     loadImage(url) {
@@ -90,29 +154,6 @@ class Player {
         l.src = url
 
         return l;
-    }
-    
-    loadIntoGame(map) {
-        loadFile("game/levels/earth.txt");
-        const mapData = map.innerHTML;
-        var filteredMapData = []
-        var indexes = [];
-        for (var i = 0; i < mapData.length; i++) {
-            if (mapData[i] = ":") {
-                indexes.push(i);
-            }
-        } for (var i = 0; i < indexes.length; i++) {
-            var data = mapData.slice(indexes[i] + 1, indexes[i + 1]);
-            var outputNum = 0;
-            for (var j = 0; j < data.length; j++) {
-                const number = data[j];
-                outputNum *= 10;
-                outputNum += number;//no support for decimals :(
-            }
-            filteredMapData.push(outputNum);
-        }
-        //also add image pathz or something.
-        return filteredMapData;
     }
 
     loadImages(paths) {
@@ -124,7 +165,11 @@ class Player {
     }
 
     draw(pos) {
-        ctx.drawImage(this.curImage, this.pos[0] - this.skinData[0] / 2, this.pos[1] - this.skinData[1] / 2, this.skinData[0], this.skinData[1]);
+        ctx.save();
+        ctx.translate(this.pos[0], this.pos[1]);
+        ctx.rotate(this.direction * Math.PI / 180);
+        ctx.drawImage(this.curImage, -this.skinData[0] / 2, - this.skinData[1] / 2, this.skinData[0], this.skinData[1]);
+        ctx.restore();
         this.loaded = true;
         //ctx.drawImage(img, 100, 100, 100, 100);
     }
@@ -143,16 +188,34 @@ class Player {
         this.pos[0] = Math.round(this.pos[0] / this.stats[1]) * this.stats[1];
         this.pos[1] = Math.round(this.pos[1] / this.stats[1]) * this.stats[1];
     }
+
+    findPosAngle(pos) {
+        /*const angleX = this.pos[0] - pos[0];
+        const angleY = this.pos[1] - pos[1];
+        const whole = Math.abs(angleX) + Math.abs(angleY)
+        const circle = 1 / Math.dist(0, 0, angleX / whole, angleY / whole);*/
+        this.direction = angle(this.pos[0], this.pos[1], pos[0], pos[1]) + 90;
+        
+    }
+
+    shoot(pos) {
+        const angleX = this.pos[0] - pos[0];
+        const angleY = this.pos[1] - pos[1];
+        const whole = Math.abs(angleX) + Math.abs(angleY)
+        const circle = 1 / Math.dist(0, 0, angleX / whole, angleY / whole);
+        let curBullet = new Bullet(this.bulletSkins, [this.stats[3], this.stats[4]], this.stats[2], this.stats[5]);
+    }
 }
 
 //let player = new Player("game/sprites/gamemakor.JPG", [10, 10]);
 //player.loadIntoGame();
 
-let player = new Player(["/game/sprites/spaceships/spaceshipA/spaceship0.png", "/game/sprites/spaceships/spaceshipA/spaceship1.png", "/game/sprites/spaceships/spaceshipA/spaceship2.png"], [32, 64], [10, 0.25, 10], 20);
+let player = new Player(["/game/sprites/spaceships/spaceshipA/spaceship0.png", "/game/sprites/spaceships/spaceshipA/spaceship1.png", "/game/sprites/spaceships/spaceshipA/spaceship2.png"], [32, 64], [5, 0.25, 4, 4, 8, 10], 20, ["game/sprites/bullets/playerBullets/bulletA/bulletshoot0", "game/sprites/bullets/playerBullets/bulletA/bulletshoot1"]);
 
 function main() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.draw();
+    player.findPosAngle(curMousePos);
     player.update(curMousePos);
     //canvas.width = window.innerWidth;
     //canvas.height = window.innerHeight;
