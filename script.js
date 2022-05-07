a176d7c46260e1d5697b63337a76b45f3f37772b
Ivan Chang <80505615+JustChangHTML5@users.noreply.github.com>
2022-05-07 03:03:17 +0000

    pew pew

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var map = document.getElementById("map");
+var gameMusic = document.getElementById("music");
+gameMusic.loop = true;
+
+//var music = new Audio("/game/music/maintheme.mp3");
+//music.play();
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -9,10 +14,10 @@ ctx.imageSmoothingEnabled = false;
 var loaded = false;
 var curMousePos = [0, 0];
 
-Math.dist=function(x1,y1,x2,y2){ 
-  if(!x2) x2=0; 
-  if(!y2) y2=0;
-  return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)); 
+Math.dist = function(x1, y1, x2, y2) {
+    if (!x2) x2 = 0;
+    if (!y2) y2 = 0;
+    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
 function rotate(cx, cy, x, y, angle) {
@@ -25,12 +30,25 @@ function rotate(cx, cy, x, y, angle) {
 }
 
 function angle(cx, cy, ex, ey) {
-  var dy = ey - cy;
-  var dx = ex - cx;
-  var theta = Math.atan2(dy, dx); // range (-PI, PI]
-  theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
-  //if (theta < 0) theta = 360 + theta; // range [0, 360)
-  return theta;
+    var dy = ey - cy;
+    var dx = ex - cx;
+    var theta = Math.atan2(dy, dx); // range (-PI, PI]
+    theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+    //if (theta < 0) theta = 360 + theta; // range [0, 360)
+    return theta;
+}
+
+function angle2(cx, cy, ex, ey, lastTheta) {
+    var dy = ey - cy;
+    var dx = ex - cx;
+    if (Math.abs(dx) <= 2 || Math.abs(dy) <= 2) {
+        return lastTheta - 90;
+    } else {// fix
+        var theta = Math.atan2(dy, dx); // range (-PI, PI]
+        theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+        //if (theta < 0) theta = 360 + theta; // range [0, 360)
+        return theta;
+    }
 }
 
 var bullets = [];
@@ -50,26 +68,30 @@ function handleMouseMove(event) {
         body = eventDoc.body;
 
         event.pageX = event.clientX +
-          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-          (doc && doc.clientLeft || body && body.clientLeft || 0);
+            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+            (doc && doc.clientLeft || body && body.clientLeft || 0);
         event.pageY = event.clientY +
-          (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-          (doc && doc.clientTop  || body && body.clientTop  || 0 );
+            (doc && doc.scrollTop || body && body.scrollTop || 0) -
+            (doc && doc.clientTop || body && body.clientTop || 0);
     }
 
     curMousePos = [event.clientX, event.clientY]
 }
 
-function loadFile(file)
-{
+canvas.addEventListener('click', function(evt) {
+    player.shoot(curMousePos); 
+});
+
+canvas.addEventListener('contextmenu', function(evt) {
+    evt.preventDefault();
+});
+
+function loadFile(file) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
                 map.innerHTML = allText;
             }
@@ -79,7 +101,7 @@ function loadFile(file)
 }
 
 class Bullet {
-    constructor(skin, skinData, stats, skinSpeed) {
+    constructor(skin, skinData, stats, skinSpeed, velocity, startPos, parent, direction) {
         this.sprites = [];
         this.skin = skin;
         this.loadImages(skin);
@@ -87,12 +109,14 @@ class Bullet {
         this.curImage = this.sprites[0];
         this.skinData = skinData
         this.stats = stats;
-        this.pos = [0, 0];
-        this.vel = [];
+        this.pos = startPos;
+        this.vel = velocity;
+        this.direction = direction;
         this.frameRate = skinSpeed;
         this.maxFrames = skinSpeed;
         this.loaded = false;
         this.destroyed = false;
+        this.parent = parent;
     }
 
     loadImage(url) {
@@ -110,20 +134,30 @@ class Bullet {
         }
     }
 
-    draw(angle) {
+    draw() {
         ctx.save();
         ctx.translate(this.pos[0], this.pos[1]);
-        ctx.rotate(angle * Math.PI / 180);
+        ctx.rotate(this.direction * Math.PI / 180);
         ctx.drawImage(this.curImage, -this.skinData[0] / 2, - this.skinData[1] / 2, this.skinData[0], this.skinData[1]);
         ctx.restore();
         this.loaded = true;
     }
 
     update() {
-        if (this.destroyed) {
-            bullets.splice(bullets.find(this), 1);
+        this.pos[0] += this.vel[0];
+        this.pos[1] += this.vel[1];
+        if (this.frameRate <= 0) {
+            this.frameRate = this.maxFrames;
+            this.curImageNum++;
+            this.curImage = this.sprites[this.curImageNum % this.sprites.length];
+        } else {
+            this.frameRate--;
         }
-        
+
+        if (this.destroyed) {
+            this.parent.bullets.splice(this.parent.bullets.indexOf(this), 1);
+        }
+
         if (this.pos[0] > canvas.width || this.pos[0] < 0) {
             this.destroyed = true;
         } else if (this.pos[1] > canvas.height || this.pos[1] < 0) {
@@ -133,7 +167,7 @@ class Bullet {
 }
 
 class Player {
-    constructor(skin, skinData, stats, skinSpeed, bulletSkins) {
+    constructor(skin, skinData, stats, skinSpeed, bulletSkins, bulletSpeed) {
         this.sprites = [];
         this.skin = skin;
         this.loadImages(skin);
@@ -141,14 +175,16 @@ class Player {
         this.curImage = this.sprites[0];
         this.skinData = skinData
         this.stats = stats;
-        this.pos = [0, 0];
+        this.pos = [canvas.width / 2, canvas.height / 2];
         this.direction = 0;
         this.frameRate = skinSpeed;
         this.maxFrames = skinSpeed;
         this.loaded = false;
+        this.bullets = [];
         this.bulletSkins = bulletSkins;
+        this.bulletSpeed = bulletSpeed;
     }
-    
+
     loadImage(url) {
         let l = new Image();
         l.src = url
@@ -173,11 +209,14 @@ class Player {
         this.loaded = true;
         //ctx.drawImage(img, 100, 100, 100, 100);
     }
-    
+
 
     update(pos) {
-        this.pos[0] += (pos[0] - this.pos[0]) / this.stats[0];
+        if (Math.abs(pos[0] - this.pos[0]) > 2 && Math.abs(pos[1] - this.pos[1]) > 2) {
+            this.pos[0] += (pos[0] - this.pos[0]) / this.stats[0];
         this.pos[1] += (pos[1] - this.pos[1]) / this.stats[0];
+        } 
+        
         if (this.frameRate <= 0) {
             this.frameRate = this.maxFrames;
             this.curImageNum++;
@@ -194,8 +233,17 @@ class Player {
         const angleY = this.pos[1] - pos[1];
         const whole = Math.abs(angleX) + Math.abs(angleY)
         const circle = 1 / Math.dist(0, 0, angleX / whole, angleY / whole);*/
-        this.direction = angle(this.pos[0], this.pos[1], pos[0], pos[1]) + 90;
-        
+        this.direction = angle2(this.pos[0], this.pos[1], pos[0], pos[1], this.direction) + 90;
+
+    }
+
+    findPosAngle2becauseIamtoolazytomakeituseful(pos) {
+        /*const angleX = this.pos[0] - pos[0];
+        const angleY = this.pos[1] - pos[1];
+        const whole = Math.abs(angleX) + Math.abs(angleY)
+        const circle = 1 / Math.dist(0, 0, angleX / whole, angleY / whole);*/
+        return angle(this.pos[0], this.pos[1], pos[0], pos[1]) + 90;
+
     }
 
     shoot(pos) {
@@ -203,26 +251,39 @@ class Player {
         const angleY = this.pos[1] - pos[1];
         const whole = Math.abs(angleX) + Math.abs(angleY)
         const circle = 1 / Math.dist(0, 0, angleX / whole, angleY / whole);
-        let curBullet = new Bullet(this.bulletSkins, [this.stats[3], this.stats[4]], this.stats[2], this.stats[5]);
+        let curBullet = new Bullet(this.bulletSkins, [this.stats[3], this.stats[4]], this.stats[2], this.stats[5], [-angleX / whole * circle * this.bulletSpeed, -angleY / whole * circle * this.bulletSpeed], [this.pos[0], this.pos[1]], this, this.findPosAngle2becauseIamtoolazytomakeituseful(pos));
+        this.bullets.push(curBullet);
+    }
+
+    updateBullets() {
+        for (var i = 0; i < this.bullets.length; i++) {
+            var curBullet = this.bullets[i];
+            curBullet.update();
+        }
+        for (var i = 0; i < this.bullets.length; i++) {
+            var curBullet = this.bullets[i];
+            curBullet.draw();
+        }
     }
 }
 
 //let player = new Player("game/sprites/gamemakor.JPG", [10, 10]);
 //player.loadIntoGame();
 
-let player = new Player(["/game/sprites/spaceships/spaceshipA/spaceship0.png", "/game/sprites/spaceships/spaceshipA/spaceship1.png", "/game/sprites/spaceships/spaceshipA/spaceship2.png"], [32, 64], [5, 0.25, 4, 4, 8, 10], 20, ["game/sprites/bullets/playerBullets/bulletA/bulletshoot0", "game/sprites/bullets/playerBullets/bulletA/bulletshoot1"]);
-
+let player = new Player(["/game/sprites/spaceships/spaceshipA/spaceship0.png", "/game/sprites/spaceships/spaceshipA/spaceship1.png", "/game/sprites/spaceships/spaceshipA/spaceship2.png"], [32, 64], [10, 0.25, 4, 8, 16, 10], 20, ["game/sprites/bullets/playerBullets/bulletA/bulletshoot0.png", "game/sprites/bullets/playerBullets/bulletA/bulletshoot1.png"], 10);
 function main() {
+    gameMusic.play();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.draw();
     player.findPosAngle(curMousePos);
     player.update(curMousePos);
+    player.updateBullets();
     //canvas.width = window.innerWidth;
     //canvas.height = window.innerHeight;
     if (player.loaded) {
         player.loaded = false;
     }
-    window.requestAnimationFrame(main)
+    window.requestAnimationFrame(main);
 }
 
 main()
